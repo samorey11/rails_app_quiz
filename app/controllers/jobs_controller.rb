ea64109bb7d5b4cb44ffbce7295b19c2276aefb8
samorey11 <samorey@outlook.com>
2022-11-08 23:06:03 -0500

    second commit

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,16 @@
 class JobsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  
   def index
-    render 'index'
+    @jobs = Job.all 
+    render 'jobs/index'
+  end
+
+  def change
+    @job = Job.new(job_params)
+
+    if @jobs.save
+      render 'jobs/create'
+    end
   end
 end
